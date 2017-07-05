bf19ae3efc6fb7908acc591fe70930d11cb5972b
Ognyan Angelov <ognyan_angelov@yahoo.com>
2017-07-06 01:11:45 +0300

    Play the inro LevelSequence; More includes

--- Source/InteractiveCut/Private/InteractiveCutBPLibrary.cpp
+++ Source/InteractiveCut/Private/InteractiveCutBPLibrary.cpp
@@ -3,12 +3,15 @@
 #include "InteractiveCutBPLibrary.h"
 #include "InteractiveCut.h"
 
+#include <Core.h>
+#include "Runtime/Engine/Classes/GameFramework/Actor.h"
 #include "Runtime/Engine/Classes/GameFramework/PlayerInput.h"
 #include "Runtime/Engine/Classes/GameFramework/InputSettings.h"
 #include "Runtime/CoreUObject/Public/UObject/UObjectGlobals.h"
-#include "Runtime/LevelSequence/Public/LevelSequence.h"
 #include "Runtime/LevelSequence/Public/LevelSequencePlayer.h"
 #include "Runtime/LevelSequence/Public/LevelSequenceActor.h"
+#include "Runtime/MovieScene/Public/MovieSceneSequencePlayer.h"
+#include "Runtime/LevelSequence/Public/LevelSequence.h"
 
 UInteractiveCutBPLibrary::UInteractiveCutBPLibrary(const FObjectInitializer& ObjectInitializer)
 : Super(ObjectInitializer)
@@ -19,10 +22,14 @@ UInteractiveCutBPLibrary::UInteractiveCutBPLibrary(const FObjectInitializer& Obj
 float UInteractiveCutBPLibrary::InteractiveCutMoment(const FName& IntroSequenceName, const FName& SuccessSequenceName, const FName& FallbackSequenceName, const FTimespan& ReactionTime, const FInputActionKeyMapping& SuccessKey)
 {
 	FStringAssetReference SequenceName(IntroSequenceName.ToString());
-
-	// ULevelSequence* Asset = Cast<ULevelSequence>(SequenceName.TryLoad());
-	// ULevelSequencePlayer::CreateLevelSequencePlayer();
-
+	ULevelSequence* Asset = Cast<ULevelSequence>(SequenceName.TryLoad());
+	UWorld* World = GEngine->GetWorld();
+	FMovieSceneSequencePlaybackSettings settings;
+	//  make_unique<ULevelSequencePlayer>(_)
+	auto player = ULevelSequencePlayer::CreateLevelSequencePlayer(World, Asset, settings);
+	
+	player->Play();
+	
 	return -1;
 }
 
