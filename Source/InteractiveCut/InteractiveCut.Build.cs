8da6fb1da94a7179b2cb3b97cb186cc2c6cc0c33
Ognyan Angelov <ognyan_angelov@yahoo.com>
2017-07-05 13:54:16 +0300

    UE4 Plugin Init

---
+++ Source/InteractiveCut/InteractiveCut.Build.cs
@@ -0,0 +1,59 @@
+// Some copyright should be here...
+
+using UnrealBuildTool;
+
+public class InteractiveCut : ModuleRules
+{
+	public InteractiveCut(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				"InteractiveCut/Public"
+				
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"InteractiveCut/Private",
+				
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+                "LevelSequence",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
---
+++ Source/InteractiveCut/Private/InteractiveCutBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "InteractiveCutBPLibrary.h"
+#include "InteractiveCut.h"
+
+UInteractiveCutBPLibrary::UInteractiveCutBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UInteractiveCutBPLibrary::InteractiveCutSampleFunction(float Param)
+{
+	return -1;
+}
+
